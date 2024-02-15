8860788bbfe3a1b537c6d967cdcc5b390be555ac
pyshen <pyshen.watson@gmail.com>
2024-02-15 18:45:20 +0800

    Update get_lfw_people

--- dataset/data_module.py
+++ dataset/data_module.py
@@ -1,6 +1,5 @@
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
-from dataset.dataset import get_lfw_people
 
 class FRDataModule(pl.LightningDataModule):
     
@@ -23,24 +22,3 @@ class FRDataModule(pl.LightningDataModule):
 
     def test_dataloader(self):
         return DataLoader(self.test_set, batch_size=self.batch_size, num_workers=self.num_workers)
-
-# Below is an example of how to use the data module
-
-if __name__ == '__main__':
-    
-    import torchvision.transforms as T
-    
-    data = get_lfw_people(train_ratio=0.8, 
-                          transform=T.Compose([
-                              T.Resize(112), 
-                              T.ToTensor()]))
-
-    lfw_data_module = FRDataModule(
-        data=data, 
-        batch_size=32, 
-        num_workers=4)
-    
-    train_loader = lfw_data_module.train_dataloader()
-    x, y = next(iter(train_loader))
-    print(x.dtype, y.dtype)
-    print(x.shape, y.shape)
--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,15 +1,36 @@
 from torchvision.datasets import LFWPeople
 from torch.utils.data import random_split
+from torchvision import transforms as T
 
-def get_lfw_people(train_ratio, transform=None):
+
+def get_lfw_people(config):
     
-    def get_split(split):
-        return LFWPeople(root='dataset', 
-                         split=split, 
+    transform_train = T.Compose([
+        T.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5),
+        T.RandomHorizontalFlip(),
+        T.RandomEqualize(),
+        T.RandomRotation(10),
+        T.RandomResizedCrop(config.input_size),
+        T.ToTensor()
+    ])
+    
+    transform_test = T.Compose([
+        T.Resize(config.input_size), 
+        T.ToTensor()
+    ])
+    
+    if not config.DA:
+        transform_train = transform_test
+    
+    train_set = LFWPeople(root='dataset', 
+                         split='train', 
                          download=True, 
-                         transform=transform)
-        
-    train_set = get_split('train')
-    test_set = get_split('test')
-    train_set, val_set = random_split(train_set, [train_ratio, 1-train_ratio])
+                         transform=transform_train)
+    
+    test_set = LFWPeople(root='dataset', 
+                         split='test', 
+                         download=True, 
+                         transform=transform_test)
+    
+    val_set, test_set = random_split(test_set, [config.val_test_ratio, 1-config.val_test_ratio])
     return train_set, val_set, test_set

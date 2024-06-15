fba1532f06d1dc7528e00f1fb2bafba7253522a5
pyshen <pyshen.watson@gmail.com>
2024-06-15 19:11:53 +0800

    Modify the name of attack model in the main MIA programs and add a requirements.txt

--- mia_eval.py
+++ mia_eval.py
@@ -1,15 +1,15 @@
 import pytorch_lightning as pl
 
 from model import TargetModel, TargetType
-from model import ShadowModel, ShadowType
-from model import ReconstructModule
+from model import AttackModel, AttackType
+from model import ModelInversionAttackModule
 from dataset import FaceDataModule
 
 pl.seed_everything(42, workers=True)
 
-target = TargetModel(model_type=TargetType.MBF_LARGE_V1).load('weights/backbone/mbf_large_v1.pt').freeze().eval()
-shadow = ShadowModel(model_type=ShadowType.IDIAP).load('weights/shadow/mbf_large_v1+idiap.pt').set_target(target)
-model = ReconstructModule(target, shadow)
+target = TargetModel(model_type=TargetType.MBF_LARGE_V1).load('weights/target/mbf_large_v1.pt').freeze().eval()
+shadow = AttackModel(model_type=AttackType.IDIAP).load('mbf_large_v1+idiap.pt').set_target(target)
+model = ModelInversionAttackModule(target, shadow)
 
 dm = FaceDataModule('../MIA/cropped_data/lfw', batch_size=128, train_ratio=0.8)
 
--- mia_train.py
+++ mia_train.py
@@ -2,19 +2,19 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 
 from model import TargetModel, TargetType
-from model import ShadowModel, ShadowType
-from model import ReconstructModule
+from model import AttackModel, AttackType
+from model import ModelInversionAttackModule
 from dataset import FaceDataModule
 
 pl.seed_everything(42, workers=True)
 
-target = TargetModel(model_type=TargetType.MBF_LARGE_V1).load('weights/backbone/mbf_large_v1.pt').freeze().eval()
-shadow = ShadowModel(model_type=ShadowType.IDIAP).set_target(target)
-model = ReconstructModule(target, shadow)
+target = TargetModel(model_type=TargetType.MBF_LARGE_V1).load('weights/target/mbf_large_v1.pt').freeze().eval()
+shadow = AttackModel(model_type=AttackType.IDIAP, dssim_weight=0.25, id_weight=0.25).set_target(target)
+model = ModelInversionAttackModule(target, shadow)
 
-dm = FaceDataModule('../MIA/cropped_data/lfw', batch_size=128, train_ratio=0.8)
+dm = FaceDataModule('../MIA/cropped_data/lfw', batch_size=160, train_ratio=0.8)
 checkpoint_callback = ModelCheckpoint(monitor='val_loss', mode='min', save_top_k=1, verbose=True)
-early_stopping_callback = EarlyStopping(monitor='val_loss', patience=3, strict=False, verbose=True, mode='min')
+early_stopping_callback = EarlyStopping(monitor='val_loss', patience=5, strict=False, verbose=True, mode='min')
 
 trainer = pl.Trainer(max_epochs=200, benchmark=True, callbacks=[checkpoint_callback, early_stopping_callback])
 trainer.fit(model, dm.train_dataloader(), dm.val_dataloader())
